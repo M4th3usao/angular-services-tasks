21d74e541cae326b0f8e7c9f36a0d90598356b56
M4th3usao <88057724+M4th3usao@users.noreply.github.com>
2022-11-04 20:23:13 -0300

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { HomeComponent } from './components/home/home.component';
+import { TimerService } from './services/timer.service';
+import { TasksService } from './services/tasks.service';
+import { RouterModule } from '@angular/router';
 import { TimerComponent } from './components/timer/timer.component';
 import { TasksComponent } from './components/tasks/tasks.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
@@ -21,6 +23,6 @@ import { RouterModule } from '@angular/router';
   ],
   declarations: [AppComponent, HomeComponent, TimerComponent, TasksComponent],
   bootstrap: [AppComponent],
-  providers: [TasksComponent, TimerComponent],
+  providers: [TasksService, TimerService],
 })
 export class AppModule {}
