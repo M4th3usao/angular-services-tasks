21b8e83606fa1a1bbcfba82765f62ea3dbc856b4
M4th3usao <88057724+M4th3usao@users.noreply.github.com>
2022-11-09 15:28:42 -0300

    3 files modified

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { ListService } from '../../services/list.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
+  constructor(public lists: ListService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
