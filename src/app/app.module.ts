e9fb6c817d1db9e2a6131f6e96487f47562638c5
M4th3usao <88057724+M4th3usao@users.noreply.github.com>
2022-11-09 14:43:38 -0300

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { TimerComponent } from './components/timer/timer.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TasksService } from './services/tasks.service';
 import { TimerService } from './services/timer.service';
+import { ListService } from './services/list.service';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   imports: [
@@ -19,10 +21,11 @@ import { TimerService } from './services/timer.service';
       { path: '', component: HomeComponent },
       { path: 'timer', component: TimerComponent },
       { path: 'tasks', component: TasksComponent },
+      { path: 'list', component: ListComponent },
     ]),
   ],
   declarations: [AppComponent, HomeComponent, TimerComponent, TasksComponent],
   bootstrap: [AppComponent],
-  providers: [TasksService, TimerService],
+  providers: [TasksService, TimerService, ListService],
 })
 export class AppModule {}
